e6f5a08128ebe3a51ac2fd8b6e3196fa81a152a5
Phillip Burleigh <pburle2@illinois.edu>
2021-12-09 12:12:40 -0600

    Add CLI input to bfs exe

--- bfs.cpp
+++ bfs.cpp
@@ -4,7 +4,12 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc != 2) { 
+    cout << "Incorrect number of arguments (need 1)." << endl;
+    return 0;
+  }
+
   CSVParser parser;
   vector<Streamer> output;
   output = parser.parseFile("data/modified_english_target.csv",
@@ -12,8 +17,13 @@ int main() {
   Graph g;
   int numNodes = Graph::kNumNodes;
   g = Graph(output, numNodes);
+  
+  int id = stoi(argv[1]);
 
-  vector<string> traversal = g.BFS(0);
+  if (id < 0 || id >= numNodes) cout << "Invalid id." << endl;
+
+  vector<string> traversal = g.BFS(id);
   utils::printPath(traversal);
+  cout << "Number of nodes in connected component: " << traversal.size() << endl;
   return 0;
 }
